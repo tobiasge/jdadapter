cd1a90be72e7eeaaf9cfe2e1f32ad7775ac6e3eb
Tobias Genannt <tobias.genannt@googlemail.com>
2014-11-22 13:02:55 +0100

    - Changed sample URLs to new requirement

--- src/main/bin/JdAdapter.sh
+++ src/main/bin/JdAdapter.sh
@@ -3,7 +3,7 @@
 ############################### S E T T I N G S ###############################
 username="username"
 password="password"
-synoUri="http://192.168.178.30:5000/download/download_redirector.cgi"
+synoUri="http://192.168.178.30:5000"
 ###############################################################################
 
 java -DsynoUri="$synoUri" -Dpassword="$password" -Dusername="$username" -jar winstone-0.9.10.jar --httpPort=9666 --warfile=JdAdapter.war
