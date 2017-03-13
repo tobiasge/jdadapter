c0007500f565ce4d3eec11105c2bcba534fc380d
Tobias Genannt <tobias.genannt@gmail.com>
2017-03-13 19:51:11 +0100

    Using winstone 3.4

--- src/main/bin/JdAdapter.bat
+++ src/main/bin/JdAdapter.bat
@@ -3,7 +3,7 @@
 REM ############################ S E T T I N G S ###################################
 set username=username
 set password=password
-set synoUri=http://192.168.178.30:5000
+set synoUri=https://diskstation-hostname:5001/
 REM ################################################################################
 
-java -DsynoUri="%synoUri%" -Dpassword="%password%" -Dusername="%username%" -jar winstone-boot-1.7.0.jar --httpPort=9666 --warfile=JdAdapter.war
+java -DsynoUri="%synoUri%" -Dpassword="%password%" -Dusername="%username%" -jar winstone-3.4.jar --httpPort=9666 --warfile=JdAdapter.war
--- src/main/bin/JdAdapter.sh
+++ src/main/bin/JdAdapter.sh
@@ -3,7 +3,7 @@
 ############################### S E T T I N G S ###############################
 username="username"
 password="password"
-synoUri="http://192.168.178.30:5000"
+synoUri="https://diskstation-hostname:5001/"
 ###############################################################################
 
-java -DsynoUri="$synoUri" -Dpassword="$password" -Dusername="$username" -jar winstone-boot-1.7.0.jar --httpPort=9666 --warfile=JdAdapter.war
+java -DsynoUri="$synoUri" -Dpassword="$password" -Dusername="$username" -jar winstone-3.4.jar --httpPort=9666 --warfile=JdAdapter.war
